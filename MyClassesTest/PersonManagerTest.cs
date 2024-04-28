96939777106bb052d8bd76b472af9e6a0dd905ef
Fernanda Nery <fernanda.a.nery@hotmail.com>
2024-04-28 20:56:59 -0300

    Criando teste para PersonManager apos analose com Code Coverage

---
+++ MyClassesTest/PersonManagerTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses.PersonClasses;
+using System;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class PersonManagerTest
+    {
+        [TestMethod]
+        public void CreatePerson_OfTypeEmployeeTest()
+        {
+            PersonManager personManagaer = new PersonManager();
+            Person person;
+
+            person = personManagaer.CreatePerson("Fernanda", "Nery", false);
+
+            Assert.IsInstanceOfType(person, typeof(Employee));
+
+        }
+    }
+}

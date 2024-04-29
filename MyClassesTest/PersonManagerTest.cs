f666bdc5e9d7a53c330b28ba86a7619f42908c04
Fernanda Nery <fernanda.a.nery@hotmail.com>
2024-04-28 21:02:56 -0300

    Criando teste para supervisor após analise com code coverage

--- MyClassesTest/PersonManagerTest.cs
+++ MyClassesTest/PersonManagerTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyClasses.PersonClasses;
 using System;
+using System.Collections.Generic;
 
 namespace MyClassesTest
 {
@@ -8,6 +9,7 @@ namespace MyClassesTest
     public class PersonManagerTest
     {
         [TestMethod]
+        [Owner("FernandaN")]
         public void CreatePerson_OfTypeEmployeeTest()
         {
             PersonManager personManagaer = new PersonManager();
@@ -16,6 +18,23 @@ namespace MyClassesTest
             person = personManagaer.CreatePerson("Fernanda", "Nery", false);
 
             Assert.IsInstanceOfType(person, typeof(Employee));
+
+        }
+
+        [TestMethod]
+        [Owner("FernandaN")]
+        public void DoEmployExistTest()
+        {
+            Supervisor supervisor = new Supervisor();
+
+            supervisor.Employees = new List<Employee>();
+            supervisor.Employees.Add(new Employee()
+            {
+                FirstName = "Fernanda",
+                LastName = "Nery"
+            });
+
+            Assert.IsTrue(supervisor.Employees.Count > 0);
 
         }
     }
